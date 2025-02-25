b093f5a0d4564d7f9d77d58aa2ab884a82d6cfcb
sangw0n <pilwon070127@naver.com>
2025-02-26 00:39:22 +0900

    추가 | 기초 프로젝트 세팅

---
+++ Assets/03. Sprites/Background/6. Particle/Particle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 0.65f);
+    }
+}
